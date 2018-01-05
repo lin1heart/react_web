27f2c7961f6dbd709c91a40d10f35088cbf019ea
lijiajia <lijiajia@jhuan.com>
2018-01-05 11:39:54 +0800

    add imageList type

--- src/models/imageList.js
+++ src/models/imageList.js
@@ -3,8 +3,25 @@ import fetch from '../services/fetch';
 const SERVER_URL ='http://192.168.0.106:8080/'
 const IMAGE_URL = 'http://192.168.0.105:80/image/';
 
+type ImageList = {
+  id: number,
+  title: string,
+  headImage: stirng,
+  type: number,
+  uploadDt?: string,
+}
+const _imageList : ImageList = [
+  // {headImage: 'https://reactjs.org/logo-og.png'},
+  // {headImage: 'http://img2.3lian.com/2014/f5/158/d/86.jpg'},
+  // {headImage: 'http://pic.sc.chinaz.com/files/pic/pic9/201508/apic14052.jpg'},
+  // {headImage: 'http://www.quanjing.com/image/2017index/lx3.png'},
+  // {headImage: 'http://p2.wmpic.me/article/2017/04/05/1491360811_KGDMrHbN_215x185.jpg'},
+  // {headImage: 'https://reactjs.org/logo-og.png'},
+  // {headImage: 'http://img2.3lian.com/2014/f5/158/d/86.jpg'},
+  // {headImage: 'http://p2.wmpic.me/article/2017/04/22/1492839241_nciiNHzu_215x185.jpg'}
+]
 const initialState = {
-  imageList: [],
+  imageList: _imageList,
   ratioList: [],
 };
 const reducers = {
@@ -30,11 +47,11 @@ const effects = {
       console.log(' res is ', imageList);
       const ratioList = yield call(calAspectRatio, imageList)
       console.log('ratioList.slice(0,4) is ', ratioList.slice(0,4),ratioList);
-      
+
       const imageHeight = yield calHeight(ratioList.slice(0,4))
       const imageHeight2 = yield calHeight(ratioList.slice(4,7))
       console.log("heigt:!!!",imageHeight,imageHeight2);
-      
+
       yield put({ type:'loadImageList', imageList})
       yield put({ type:'loadRatioList', payload:{ ratioList }})
     } catch (e) {
@@ -55,12 +72,12 @@ function calAspectRatio(data) {
     image.src = IMAGE_URL + data[index].headImage;
     image.onload = (()=>{
       console.log('onload');
-      
+
       ratioList[index] = image.width/image.height;
     })
   })
   console.log("ratioList222222 is ",ratioList);
-  
+
       return ratioList;
 }
 function calHeight(arr){
@@ -71,8 +88,8 @@ function calHeight(arr){
   })
   let heig = body_width/aspectRatioSum;
   console.log("aspectRatioSum",aspectRatioSum,body_width,arr);
-  
-  return heig; 
+
+  return heig;
 }
 // async function aa (data) {
 //   const ratioList = [];
