7cada7fbf86528467f554d7d08720a0a5ec0adb4
lijiajia <lijiajia@jhuan.com>
2018-01-12 13:55:18 +0800

    temp

---
+++ src/services/api.js
@@ -0,0 +1,1 @@
+import { SERVER_URL, IMAGE_URL } from './api'
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,5 @@
-const SERVER_URL ='http://54.179.154.58:8080/';
-const IMAGE_URL = 'http://54.179.154.58:8888/dbImage/';
-const UPLOAD_URL = 'ftp://54.179.154.58:8888/image/upload/';
+const SERVER_URL = "http://54.179.154.58:8080/";
+const IMAGE_URL = "http://54.179.154.58:8888/dbImage/";
+const UPLOAD_URL = "ftp://54.179.154.58:8888/";
 
-export {
-    SERVER_URL,
-    IMAGE_URL,
-    UPLOAD_URL
-}
+export { SERVER_URL, IMAGE_URL, UPLOAD_URL };
