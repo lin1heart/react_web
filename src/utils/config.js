6116b157a4b669e47fbb9dd6c9db728415d74d54
lijiajia <lijiajia@jhuan.com>
2018-02-06 15:26:42 +0800

    update server ip

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://54.179.154.58:8080/';
-export const IMAGE_URL = 'http://54.179.154.58:8888/dbImage/';
-export const UPLOAD_URL = 'ftp://54.179.154.58:8888/';
+export const SERVER_URL = 'http://52.77.16.226:8080/';
+export const IMAGE_URL = 'http://52.77.16.226:8888/dbImage/';
+export const UPLOAD_URL = 'http://52.77.16.226:8888/';
